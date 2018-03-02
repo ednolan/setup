ce83f12368520a41f95b75a717a7b4cbe4e4fdff
Edward Nolan <eddiejnolan@gmail.com>
2018-03-01 22:33:45 -0500

    Add mac alias file

---
+++ linux-homedir/util/aliases-mac.sh
@@ -0,0 +1,17 @@
+# A verbose, human readable ls
+alias ll="ls -lhA"
+
+# New emacs with colors etc
+alias coloremacs="TERM=xterm-24bits /Applications/Emacs.app/Contents/MacOS/Emacs-x86_64-10_9 -nw"
+
+# This ulimit command is annoying
+alias coredumps="ulimit -S -c unlimited"
+
+# From github.com/jez/dotfiles
+# so much easier to type than `cd ..`
+alias cdd="cd .."
+alias cddd="cd ../.."
+alias cdddd="cd ../../.."
+alias cddddd="cd ../../../.."
+alias cdddddd="cd ../../../../.."
+alias cddddddd="cd ../../../../../.."
