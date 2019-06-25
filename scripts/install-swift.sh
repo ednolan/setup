f421facca6e2ee7aea13014fff09008afa882b51
Edward Nolan <eddiejnolan@gmail.com>
2019-06-24 21:26:24 -0400

    Update swift version

--- scripts/install-swift.sh
+++ scripts/install-swift.sh
@@ -2,12 +2,12 @@
 
 sudo apt-get install clang libicu-dev
 mkdir -p ~/swiftstuff
-wget -P ~/swiftstuff https://swift.org/builds/swift-4.1.1-release/ubuntu1604/swift-4.1.1-RELEASE/swift-4.1.1-RELEASE-ubuntu16.04.tar.gz
-wget -P ~/swiftstuff https://swift.org/builds/swift-4.1.1-release/ubuntu1604/swift-4.1.1-RELEASE/swift-4.1.1-RELEASE-ubuntu16.04.tar.gz.sig
+wget -P ~/swiftstuff https://swift.org/builds/swift-5.0.1-release/ubuntu1804/swift-5.0.1-RELEASE/swift-5.0.1-RELEASE-ubuntu18.04.tar.gz
+wget -P ~/swiftstuff https://swift.org/builds/swift-5.0.1-release/ubuntu1804/swift-5.0.1-RELEASE/swift-5.0.1-RELEASE-ubuntu18.04.tar.gz.sig
 wget -q -O - https://swift.org/keys/all-keys.asc | gpg --import -
 gpg --keyserver hkp://pool.sks-keyservers.net --refresh-keys Swift
 (cd ~/swiftstuff
- gpg --verify swift-4.1.1-RELEASE-ubuntu16.04.tar.gz.sig
- tar xzf swift-4.1.1-RELEASE-ubuntu16.04.tar.gz
+ gpg --verify swift-5.0.1-RELEASE-ubuntu18.04.tar.gz.sig
+ tar xzf swift-5.0.1-RELEASE-ubuntu18.04.tar.gz
 )
 
