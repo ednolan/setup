135a762b42fa69bdc8b36d2396f62c8ae6aade7e
Edward Nolan <eddiejnolan@gmail.com>
2020-03-02 16:59:54 -0500

    Work stuff

--- scripts/setuputil/configure/dotfiles.sh
+++ scripts/setuputil/configure/dotfiles.sh
@@ -9,8 +9,11 @@ declare emoji=$1
 sed -i '/##### custom suffix follows/,$d' ~/.bashrc
 echo '##### custom suffix follows' >> ~/.bashrc
 cat $script_dir/../../../linux-homedir/bashrc-suffix.$emoji >> ~/.bashrc
+if [[ -f $script_dir/../../../linux-homedir/bash_profile.$emoji ]] ; then
+    cp $script_dir/../../../linux-homedir/bash_profile.$emoji >> ~/.bash_profile
+fi
 cp $script_dir/../../../linux-homedir/bash_aliases.$emoji ~/.bash_aliases
-cp $script_dir/../../../linux-homedir/gitconfig ~/.gitconfig
+cp $script_dir/../../../linux-homedir/gitconfig.$emoji ~/.gitconfig
 cp $script_dir/../../../linux-homedir/rdmrc ~/.rdmrc
 cp $script_dir/../../../linux-homedir/tmux.conf ~/.tmux.conf
 cp -r $script_dir/../../../linux-homedir/config/. ~/.config
