5027f5dc02f4f0dcf7ed3ebdbf9aef17c9638158
Edward Nolan <eddiejnolan@gmail.com>
2020-03-03 15:19:13 -0500

    rtags

--- scripts/setuputil/localbuild/rtags.sh
+++ scripts/setuputil/localbuild/rtags.sh
@@ -6,9 +6,13 @@ if [[ -d ~/localbuild/rtags ]] ; then
     rm -rf ~/localbuild/rtags
 fi
 mkdir -p ~/localbuild/rtags
-cd ~/localbuild/rtags
-git clone --recursive https://github.com/Andersbakken/rtags.git
-cd rtags
+
+git clone --recursive https://github.com/Andersbakken/rtags.git ~/localbuild/rtags/rtags
+
+cd ~/localbuild/rtags/rtags
+
+git checkout v2.38
+
 cmake -DCMAKE_INSTALL_PREFIX=~/localbuild/rtags .
 make -j8
 make install
