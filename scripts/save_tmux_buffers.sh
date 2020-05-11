06326edde22440a996fcd28768c8945ec5581f00
Edward Nolan <enolan@maystreet.com>
2020-05-11 12:12:02 -0400

    save_tmux_buffers.sh: don't save "pw" buffer

--- scripts/save_tmux_buffers.sh
+++ scripts/save_tmux_buffers.sh
@@ -7,17 +7,19 @@ function main()
 	mkdir -p $outdir
 	local -a tmux_bufs=($(tmux ls | cut -d' ' -f1 | tr -d ':'))
 	for buf in ${tmux_bufs[@]} ; do
-		tmux a -t $buf
-		local -a windows=($(tmux list-windows | cut -d' ' -f1 | tr -d ':'))
-		for window in ${windows[@]} ; do
-			tmux select-window -t $window
-			local -a panes=($(tmux list-panes | cut -d' ' -f1 | tr -d ':'))
-			for pane in ${panes[@]} ; do
-				tmux select-pane -t $pane
-				tmux capture-pane -S -50000
-				tmux save-buffer "$outdir/${buf}-${window}-${pane}.txt"
-			done
-		done
+        if [[ $buf != "pw" ]] ; then
+		    tmux a -t $buf
+		    local -a windows=($(tmux list-windows | cut -d' ' -f1 | tr -d ':'))
+		    for window in ${windows[@]} ; do
+			    tmux select-window -t $window
+			    local -a panes=($(tmux list-panes | cut -d' ' -f1 | tr -d ':'))
+			    for pane in ${panes[@]} ; do
+				    tmux select-pane -t $pane
+				    tmux capture-pane -S -50000
+				    tmux save-buffer "$outdir/${buf}-${window}-${pane}.txt"
+			    done
+		    done
+        fi
 	done
 }
 
