79d05ac1a237e3df8b0be0b1ab3d819443084a26
Edward Nolan <eddiejnolan@gmail.com>
2021-12-15 11:49:50 -0500

    dockerize emojisplit-all

--- scripts/emojisplit-all.sh
+++ scripts/emojisplit-all.sh
@@ -2,4 +2,6 @@
 
 declare script_dir=$(dirname $BASH_SOURCE)
 
-find $script_dir/.. -name '*.in' | xargs -n1 emojisplit --split
+declare infiles_dir=$(realpath "$script_dir/..")
+
+find "$infiles_dir" -name '*.in' | xargs -n1 docker run -v "$infiles_dir:$infiles_dir" -v "$HOME/.config/emojisplit:/root/.config/emojisplit" emojisplit --split
