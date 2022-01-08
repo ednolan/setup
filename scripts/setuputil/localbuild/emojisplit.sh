411a8b9f0e5f7695cc464ca31cc6c7667dc8ba37
Edward Nolan <eddiejnolan@gmail.com>
2022-01-08 17:22:46 -0500

    emojisplit script

--- scripts/setuputil/localbuild/emojisplit.sh
+++ scripts/setuputil/localbuild/emojisplit.sh
@@ -9,7 +9,7 @@ declare emojsplit_script_dir=$(realpath $(dirname "$BASH_SOURCE"))
 function main() {
     mkdir -p "$HOME/sync/rust/"
     cd "$HOME/sync/rust/"
-    git clone git@github.com:ednolan/emojisplit.git
+    [[ -d "$HOME/sync/rust/emojisplit" ]] || git clone git@github.com:ednolan/emojisplit.git
     cd emojisplit
     cargo build --release
 }
