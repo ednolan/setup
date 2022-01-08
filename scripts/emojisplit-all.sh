e80e4ce33db8366bfdaf3a1128052014dff82cc2
Edward Nolan <eddiejnolan@gmail.com>
2022-01-08 21:40:08 +0000

    rust emojisplit and 🌐

--- scripts/emojisplit-all.sh
+++ scripts/emojisplit-all.sh
@@ -4,4 +4,4 @@ declare script_dir=$(dirname $BASH_SOURCE)
 
 declare infiles_dir=$(realpath "$script_dir/..")
 
-find "$infiles_dir" -name '*.in' | xargs -n1 docker run -v "$infiles_dir:$infiles_dir" -v "$HOME/.config/emojisplit:/root/.config/emojisplit" emojisplit --split
+find "$infiles_dir" -name '*.in' | xargs -n1 emojisplit --split
