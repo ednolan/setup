44b58c43ec15f330f1049716bdd2ac28c3348078
Edward Nolan <eddiejnolan@gmail.com>
2023-02-02 12:55:08 -0500

    clipetty wrapper aliases

--- scripts/clipetty_wrapper.sh
+++ scripts/clipetty_wrapper.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+shopt -s expand_aliases
+
+source ~/.bash_aliases
+
 TMP="$(mktemp /tmp/stdin-XXX)"
 cat >$TMP
 emacs -Q --eval="(progn (toggle-debug-on-error) (add-to-list 'load-path \"~/.emacs.d/elpa/clipetty-20200327.2241\") (require 'clipetty) (global-clipetty-mode) (find-file \"$TMP\") (mark-whole-buffer) (kill-ring-save nil nil 't) (kill-emacs))" < /dev/tty
