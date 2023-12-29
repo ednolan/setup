190925363c7fb7f7951543e390aed6a607b47b86
Edward Nolan <enolan@maystreet.com>
2023-12-28 23:20:34 -0500

    go and gojira scripts

---
+++ scripts/setuputil/install/go.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd /tmp
+wget 'https://go.dev/dl/go1.21.3.linux-amd64.tar.gz'
+sudo tar -C /usr/local -xzf go1.21.3.linux-amd64.tar.gz
---
+++ scripts/setuputil/install/gojira.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+GO111MODULE=on go install github.com/go-jira/jira/cmd/jira@v1.0.28
