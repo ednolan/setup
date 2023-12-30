5c007bb21e7f5d6f260f0656258bd90c6320fb75
Edward Nolan <eddiejnolan@gmail.com>
2023-12-30 14:18:23 -0500

    localbuild cmake

---
+++ scripts/setuputil/localbuild/cmake.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ -d ~/localbuild/cmake ]] ; then
+    rm -rf ~/localbuild/cmake
+fi
+mkdir -p ~/localbuild/cmake
+
+git clone https://gitlab.kitware.com/cmake/cmake.git ~/localbuild/cmake/cmake
+
+cd ~/localbuild/cmake/cmake
+
+git checkout v3.28.1
+
+./bootstrap --prefix=$HOME/localbuild/cmake
+make -j$(nproc)
+make install
