21ba90beb80a4b97861edd83e6c58d95af74ccbf
Edward Nolan <enolan@maystreet.com>
2025-12-29 16:31:14 -0500

    bump emacs version to 30.2

--- scripts/setuputil/localbuild/emacs.sh
+++ scripts/setuputil/localbuild/emacs.sh
@@ -5,9 +5,9 @@ if [[ -d ~/localbuild/emacs ]] ; then
 fi
 mkdir -p ~/localbuild/emacs
 cd ~/localbuild/emacs
-wget https://ftpmirror.gnu.org/emacs/emacs-30.1.tar.xz
-tar xf emacs-30.1.tar.xz
-cd emacs-30.1
+wget https://ftpmirror.gnu.org/emacs/emacs-30.2.tar.xz
+tar xf emacs-30.2.tar.xz
+cd emacs-30.2
 
     # --with-native-compilation=aot \
 
